2089844714f09b8071e18eef8a36de3ce616a10b
Anna Esteve <aesteve8@gmail.com>
2023-11-24 13:10:31 +0100

    initial status

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center w-screen h-screen">
+      <h1>Hey</h1>
+    </div>
+  );
+}
+
+export default App;
